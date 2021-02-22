c2321894515426c93ce3d8aec26760dd12a9a698
JoMa <blue-kiwi-media@web.de>
2021-02-22 10:46:39 +0100

    103-weihnachtsspiel-programmierung Aufgabe erledigt

---
+++ Assets/MyGame/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 100f;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 force = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, 0);
+        rb.AddForce(force);
+    }
+}
---
+++ Assets/MyGame/Scripts/Stats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stats : MonoBehaviour
+{
+    public float startTime = 60f;
+    public Text timer;
+
+    void Update()
+    {
+        startTime -= Time.deltaTime;
+
+        float minutes = Mathf.FloorToInt(startTime / 60);
+        float seconds = Mathf.FloorToInt(startTime % 60);
+
+        if (startTime > 0)
+        {
+            timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+
+    }
+}
